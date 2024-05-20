1ef47f47f8b2dc1eb3aef546e0ca3e40f6d2a1fe
lusheng.feng <lusheng.feng@iyouke.com>
2024-05-20 15:32:38 +0800

    feat: 分页

--- play/app.vue
+++ play/app.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { Pagination } from "@k-ui/components";
+
+const pageInfo = ref({
+  page: 1,
+  pageSize: 10,
+  total: 100,
+});
+</script>
 
 <template>
   <k-button>测试按钮</k-button>
+  <Pagination :pageInfo="pageInfo" />
 </template>
 
 <style scoped lang="less"></style>
